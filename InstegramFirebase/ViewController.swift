9c1f05c999b782a72ab859ecbe0d01ad4430da63
AHMED <ahmed@AHMEDs-MacBook-Pro.local>
2019-09-01 10:35:59 +0200

    Initial Commit

---
+++ InstegramFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstegramFirebase
+//
+//  Created by AHMED on 6/10/1398 AP.
+//  Copyright © 1398 AHMED. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
